caedf4bdd2b91957e1ea7c0094f8b6d195ff4b50
TranQuan12345546 <99300656+TranQuan12345546@users.noreply.github.com>
2023-03-16 21:48:49 +0700

    ádasd

--- main.js
+++ main.js
@@ -21,11 +21,11 @@ input3.insertAdjacentElement('beforebegin', labelConfirmPass);
 
 document.addEventListener('keydown', function() {
     btn.removeAttribute('disabled');
-                btn.style.cursor = 'pointer';
+    btn.style.cursor = 'pointer';
 })
 
 const btn = document.querySelector('#btn');
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function(event) {
     let Username = input1.value;
     let password = input2.value;
     let rePassword = input3.value;
@@ -33,8 +33,30 @@ btn.addEventListener('click', function() {
         btn.disabled = true;
         btn.style.cursor = 'not-allowed';
     } else if (password != rePassword) {
-        alert('confirm password khong dung!')
+        alert('confirm password khong dung!');
+        event.preventDefault();
     } else {
         alert(' đăng ký thành công')
     }
+})
+
+let btnHide2 = document.querySelector('.hide2');
+let btnHide3 = document.querySelector('.hide3');
+
+btnHide2.addEventListener('click', function(event) {
+    if(input2.type == 'password') {
+        input2.type = 'text';
+    } else {
+        input2.type = 'password';
+    }
+    event.preventDefault();
+})
+
+btnHide3.addEventListener('click', function(event) {
+    if(input3.type == 'password') {
+        input3.type = 'text';
+    } else {
+        input3.type = 'password';
+    }
+    event.preventDefault();
 })
